64dc5bc070f2b440d740a68d56514b876af151f6
Mirror83 <glenochieng045@gmail.com>
2025-03-17 09:51:10 +0300

    chore: added cubic function

--- Assets/Scripts/FunctionLibrary.cs
+++ Assets/Scripts/FunctionLibrary.cs
@@ -3,11 +3,12 @@ using static UnityEngine.Mathf;
 public static class FunctionLibrary {
     public delegate float Function(float x);
     
-    public enum FunctionName { Parabola, Wave }
+    public enum FunctionName { Parabola, Wave, CubicCurve }
 
     static Function[] functions = {
         Parabola,
-        Wave
+        Wave,
+        CubicCurve
     };
 
     public static Function GetFunction(FunctionName name) {
@@ -16,6 +17,10 @@ public static class FunctionLibrary {
 
     public static float Parabola(float x) {
         return x * x;
+    }
+
+    public static float CubicCurve(float x) {
+        return x * x * x;
     }
 
     public static float Wave(float x) {
