e5a02f9a12bfe4e4134cdc274e3ae5eaf911d372
Mirror83 <glenochieng045@gmail.com>
2025-03-17 12:08:06 +0300

    refactor: removed non-periodic functions
    these are not suitable for animation

--- Assets/Scripts/FunctionLibrary.cs
+++ Assets/Scripts/FunctionLibrary.cs
@@ -3,24 +3,14 @@ using static UnityEngine.Mathf;
 public static class FunctionLibrary {
     public delegate float Function(float x);
     
-    public enum FunctionName { Parabola, Wave, CubicCurve }
+    public enum FunctionName { Wave }
 
     static Function[] functions = {
-        Parabola,
         Wave,
-        CubicCurve
     };
 
     public static Function GetFunction(FunctionName name) {
         return functions[(int)name];
-    }
-
-    public static float Parabola(float x) {
-        return x * x;
-    }
-
-    public static float CubicCurve(float x) {
-        return x * x * x;
     }
 
     public static float Wave(float x) {
