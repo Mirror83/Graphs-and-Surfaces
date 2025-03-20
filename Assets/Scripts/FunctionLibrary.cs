4878dac5ddc6ddc47abbdfa3cd169c75f502fe3e
Mirror83 <glenochieng045@gmail.com>
2025-03-20 12:15:59 +0300

    feat: added MultiWave and Ripple functions

--- Assets/Scripts/FunctionLibrary.cs
+++ Assets/Scripts/FunctionLibrary.cs
@@ -3,10 +3,12 @@ using static UnityEngine.Mathf;
 public static class FunctionLibrary {
     public delegate float Function(float x, float t);
     
-    public enum FunctionName { Wave }
+    public enum FunctionName { Wave, MutliWave, Ripple }
 
     static Function[] functions = {
         Wave,
+        MutliWave,
+        Ripple
     };
 
     public static Function GetFunction(FunctionName name) {
@@ -15,5 +17,18 @@ public static class FunctionLibrary {
 
     public static float Wave(float x, float t) {
         return Sin(PI * (x + t));
+    }
+
+    public static float MutliWave(float x, float t) {
+        float y = Sin(PI * (x + 0.5f * t));
+        y += Sin(2f * PI * (x + t)) * 0.5f;
+        y *= 2f / 3f;
+        return y;
+    }
+
+    public static float Ripple(float x, float t) {
+        float d= Abs(x);
+        float y = Sin(PI * (4f * d - t));
+        return y / (1f + 10f * d);
     }
 }
