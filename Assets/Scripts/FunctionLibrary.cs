332626d2af650df5d5e8756d0de3f55e6b7f2ec6
Mirror83 <glenochieng045@gmail.com>
2025-03-24 11:14:14 +0300

    feat: added two torus functions

--- Assets/Scripts/FunctionLibrary.cs
+++ Assets/Scripts/FunctionLibrary.cs
@@ -4,13 +4,15 @@ using static UnityEngine.Mathf;
 public static class FunctionLibrary {
     public delegate Vector3 Function(float u, float v, float t);
     
-    public enum FunctionName { Wave, MutliWave, Ripple, Sphere }
+    public enum FunctionName { Wave, MutliWave, Ripple, Sphere, Torus, RotatingStarTorus }
 
     static readonly Function[] functions = {
         Wave,
         MutliWave,
         Ripple,
-        Sphere
+        Sphere,
+        Torus,
+        RotatingStarTorus
     };
 
     public static Function GetFunction(FunctionName name) {
@@ -58,6 +60,32 @@ public static class FunctionLibrary {
         float s = r * Cos(0.5f * PI * v);
         p.x = s * Sin(PI * u);
         p.y = r * Sin(PI * 0.5f * v);
+        p.z = s * Cos(PI * u);
+
+        return p;
+    }
+
+    public static Vector3 Torus(float u, float v, float t) {
+        Vector3 p;
+        float r1 = 0.75f; // Major radius
+        float r2 = 0.25f; // Minor radius
+
+        float s = r1 + r2 * Cos(PI * v);
+        p.x = s * Sin(PI * u);
+        p.y = r2 * Sin(PI * v);
+        p.z = s * Cos(PI * u);
+
+        return p;
+    }
+
+    public static Vector3 RotatingStarTorus(float u, float v, float t) {
+        Vector3 p;
+        float r1 = 0.7f + 0.1f * Sin(PI * (6f * u + 0.5f * t)); // Major radius
+		float r2 = 0.15f + 0.05f * Sin(PI * (8f * u + 4f * v + 2f * t)); // Minor radius
+
+        float s = r1 + r2 * Cos(PI * v);
+        p.x = s * Sin(PI * u);
+        p.y = r2 * Sin(PI * v);
         p.z = s * Cos(PI * u);
 
         return p;
