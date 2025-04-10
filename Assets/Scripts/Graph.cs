2f3806fb2595d9a1b0903d470b3b53ba6c34ea63
Glen Ochieng <glenochieng045@gmail.com>
2025-04-10 08:16:16 +0300

    feat: add morphing between functions

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -18,12 +18,11 @@ public class Graph : MonoBehaviour
     [SerializeField]
     TransitionMode transitionMode;
 
-    /// <summary>
-    /// The duration (in seconds) over which the current function is
-    /// displayed before being changed to the next one
-    /// </summary>
     [SerializeField, Min(0f)]
-    float functionDuration = 1f;
+    float functionDuration = 1f, transitionDuration = 1f;
+
+    bool transitioning;
+    FunctionName transitionFunctionName;
 
     float duration = 0f;
 
@@ -53,12 +52,31 @@ public class Graph : MonoBehaviour
     void Update()
     {
         duration += Time.deltaTime;
-        if (duration >= functionDuration)
+        if (transitioning)
+        {
+            if (duration >= transitionDuration)
+            {
+                duration -= transitionDuration;
+                transitioning = false;
+            }
+        }
+        else if (duration >= functionDuration)
         {
             duration -= functionDuration;
+
+            transitioning = true;
+            transitionFunctionName = functionName;
             PickNextFunction();
         }
-        UpdateCurrentFunction();
+
+        if (transitioning)
+        {
+            UpdateFunctionTransition();
+        }
+        else
+        {
+            UpdateCurrentFunction();
+        }
     }
 
     void PickNextFunction()
@@ -87,6 +105,32 @@ public class Graph : MonoBehaviour
             float u = (x + 0.5f) * step - 1f;
 
             points[i].localPosition = F(u, v, time);
+        }
+    }
+
+    void UpdateFunctionTransition()
+    {
+        Function from = GetFunction(transitionFunctionName), to = GetFunction(functionName);
+
+        float progress = duration / transitionDuration;
+
+        float time = Time.time;
+        float step = 2f / resolution;
+
+        float v = 0.5f * step - 1f;
+
+        for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++)
+        {
+            if (x == resolution)
+            {
+                x = 0;
+                z += 1;
+                v = (z + 0.5f) * step - 1f;
+            }
+
+            float u = (x + 0.5f) * step - 1f;
+
+            points[i].localPosition = Morph(u, v, time, from, to, progress);
         }
     }
 }
